e1875f01a84f652f4d016fcd052027073e03335f
a-gv <adagarvelink@me.com>
2020-02-27 16:05:46 +0100

    homepage with links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import DiscoverMoviesPage from './pages/DiscoverMoviesPage';
+import { Switch, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav>
+       <NavLink to ="/aboutpage" 
+       activeStyle={{
+    fontWeight: "bold",
+    color: "red"
+  }}>
+  About this website</NavLink>
+       <NavLink to ="/discovermoviespage"
+         activeStyle={{
+          fontWeight: "bold",
+          color: "red"
+        }}>
+        Discover movies</NavLink>
+     </nav>
+     <Switch>
+       <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+     </Switch> 
     </div>
   );
 }
---
+++ src/pages/DiscoverMoviesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DiscoverMoviesPage(){
+ return (
+   <h1>Discover Movies</h1>
+ ) 
+}
